c9ff8383f64b8125b32303c4fa8b5a11db5b19a6
Shuhei Hasegawa <shuhei@bestevermyself.be>
2016-11-28 21:14:15 +0900

    first init.

--- FlickMusicPlayer/ViewController.swift
+++ FlickMusicPlayer/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, iCarouselDataSource, iCarouselDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let carousel = iCarousel(frame: CGRect(x: 0, y: 0, width: 300, height: 200))
+        carousel.dataSource = self
+        carousel.type = .coverFlow
+        view.addSubview(carousel)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return 10
+    }
 
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        
+        let imageView: UIImageView
+        
+        if view != nil {
+            imageView = view as! UIImageView
+        } else {
+            imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 128, height: 128))
+        }
+        
+        imageView.image = UIImage(named: "page.png")
+        
+        return imageView
+        
+    }
+    
+    
+    
 }
 
